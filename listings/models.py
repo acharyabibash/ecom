e72d25de62f3bdeb016906318245efbab1789a34
Bibash Acharya <acharabibash@outlook.com>
2020-07-03 12:51:36 +0545

    Sixth : Added Groceries field to the Models

--- listings/models.py
+++ listings/models.py
@@ -7,7 +7,7 @@ class Listing(models.Model):
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100,default='California')
+    state = models.CharField(max_length=100,default='Pradesh1')
     zipcode = models.CharField(max_length=20)
     description = models.TextField(blank =True)
     price = models.IntegerField(default=0)
@@ -25,5 +25,6 @@ class Listing(models.Model):
     photo6 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank =True)
     is_published = models.BooleanField(default=True)
     list_date = models.DateTimeField(default=datetime.now, blank=True)
+    groceries = models.TextField(default="Fruits and Vegetables")
     def __str__(self):
         return self.title
